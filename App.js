02b7ba63ca664e07fe11c462b854710c6185caf6
somesh <someshsrichandan@gmail.com>
2025-04-06 22:09:11 +0530

    all images are added

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+       source={{uri: 'https://wallpaperbat.com/img/873547-useful-websites-every-front-end.png'}}
+       style={{width: '100%', aspectRatio: 16 / 9}}
+      />
+      <Image
+      source={require('./assets/me1.png')}
+      style={{width: 150, height: 150, borderRadius: 75 , borderWidth: 5, borderColor: 'white' , marginTop: -75}}
+      />
+      <Text style={{fontSize: 20 , fontWeight: 'bold'}}>Somesh Srichandan Sahoo</Text>
+      <Text>I am full stack developer</Text>
       <StatusBar style="auto" />
     </View>
   );
