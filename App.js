2a4e7f7efa64ad5a39c796ff1bc137a64a61a560
somesh <someshsrichandan@gmail.com>
2025-04-07 09:42:05 +0530

    project card added

--- App.js
+++ App.js
@@ -11,6 +11,19 @@ import {
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+
+function ProjectCard ({ name, image }) {
+  return (
+    <View>
+      <Image
+        source={image}
+        style={{ height: 150, aspectRatio: 16 / 9 , borderRadius: 10 }}
+      />
+      <Text style={{ fontSize: 16 }}>{name}</Text>
+    </View>
+  );
+}
+
 export default function App() {
   const handelPress = () => {
     Linking.openURL("mailTo:someshsrichandan@gmail.com");
@@ -52,28 +65,32 @@ export default function App() {
 
             <Text style={{ padding: 10, fontSize: 16 }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
-              aliquam sem et tortor consequat id porta nibh. Pellentesque nec
-              nam aliquam sem. Placerat duis ultricies lacus sed. Non curabitur
-              gravida arcu ac tortor dignissim convallis aenean. Amet nisl purus
-              in mollis nunc. Vel elit scelerisque mauris pellentesque pulvinar
-              pellentesque. Sagittis orci a scelerisque purus semper eget duis
-              at tellus. Sed libero enim sed faucibus turpis in eu mi bibendum.
-              Duis at consectetur lorem donec massa sapien faucibus et molestie.
-              At ultrices mi tempus imperdiet nulla malesuada pellentesque elit
-              eget. Purus sit amet volutpat consequat mauris nunc congue nisi
-              vitae. Urna condimentum mattis pellentesque id nibh tortor id.
-              Consequat id porta nibh venenatis. Lectus vestibulum mattis
-              ullamcorper velit sed ullamcorper. Mauris a diam maecenas sed enim
-              ut sem. Volutpat commodo sed egestas egestas fringilla phasellus.
-              Turpis egestas integer eget aliquet nibh praesent tristique magna
-              sit. Congue mauris rhoncus aenean vel elit scelerisque. Tellus
-              integer feugiat scelerisque varius morbi enim. Consectetur a erat
-              nam at. Bibendum arcu vitae elementum curabitur vitae nunc. Sit
-              amet consectetur adipiscing elit. Rhoncus mattis rhoncus urna
-              neque viverra justo. Malesuada pellentesque elit eget gravida.
-              Vitae nunc sed velit dignissim sodales ut eu sem integer.
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </Text>
+            <Text style={{fontWeight: "bold",marginTop: 10 , fontSize: 18}}>
+              Project
+            </Text>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                padding: 10,
+                gap: 10,
+              }}
+            >
+            <ProjectCard
+              name="Qr code generator"
+              image={require("./assets/basic.png")}
+            />
+            <ProjectCard
+              name="To Do"
+              image={require("./assets/to-do-1.png")}
+            />
+             <ProjectCard
+              name="Weatheer App"
+              image={require("./assets/weatherapp.jpg")}
+            />
+            </ScrollView>
             <StatusBar style="light" />
           </View>
         </ScrollView>
